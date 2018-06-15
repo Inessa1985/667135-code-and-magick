a3c4a4ea96c5a1b188593bfaa6cabe770afcc7d4
Inessa <panda1756@yandex.ru>
2018-06-15 18:05:40 +0300

    Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,29 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
-document.querySelector('.setup-similar').classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
-
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var userNameInput = setup.querySelector('.setup-user-name');
+
+var wizardSetup = setup.querySelector('.setup-wizard');
+var wizardCoat = wizardSetup.querySelector('.wizard-coat');
+var wizardCoatInput = setup.querySelector('input[name="coat-color"]');
+var wizardEyes = wizardSetup.querySelector('.wizard-eyes');
+var wizardEyesInput = setup.querySelector('input[name="eyes-color"]');
+var fireball = setup.querySelector('.setup-fireball-wrap');
+var fireballInput = setup.querySelector('input[name="fireball-color"]');
+
 
 var getRandomElement = function (arr) {
   return arr[Math.floor(Math.random() * (arr.length - 1))];
@@ -48,8 +58,98 @@ var creatList = function (wizardsCount) {
   return fragment;
 };
 
+
+// setup.classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
 var wizardsCount = 4;
-
 similarListElement.appendChild(creatList(wizardsCount));
+setup.querySelector('.setup-similar').classList.remove('hidden');
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+// Взаимодействие кнопок открытия и закрытия .setup
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+
+// Валидация формы
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
+
+// Изменение цвета глаз, плащей и фаерболов при нажатии на персонажи
+var fillColor = function (colorsArray, targetElement, targetInput) {
+  var color = colorsArray[getRandomElement(colorsArray)];
+
+  targetElement.style = 'fill: ' + color + ';';
+  targetInput.value = color.toString();
+};
+
+var changeBackground = function (colorsArray, targetElement, targetInput) {
+  var color = colorsArray[getRandomElement(colorsArray)];
+
+  targetElement.style = 'background-color: ' + color + ';';
+  targetInput.value = color.toString();
+};
+
+wizardCoat.addEventListener('click', function () {
+  fillColor(COAT_COLOR, wizardCoat, wizardCoatInput);
+});
+
+wizardEyes.addEventListener('click', function () {
+  fillColor(EYES_COLOR, wizardEyes, wizardEyesInput);
+});
+
+fireball.addEventListener('click', function () {
+  changeBackground(FIREBALL_COLORS, fireball, fireballInput);
+});
