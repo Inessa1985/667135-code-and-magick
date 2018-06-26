82c77e91eb18355091183f20205ca248ce90edb2
Inessa <panda1756@yandex.ru>
2018-06-26 22:56:56 +0300

    pimp my mage

--- js/library.js
+++ js/library.js
@@ -8,4 +8,16 @@
     return arr[Math.floor(Math.random() * (arr.length - 1))];
   };
 
+  // Перетасовывает массив
+  window.library.getShuffleArray = function (array) {
+    for (var i = 0; i < array.length - 1; i++) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+
+    return array;
+  };
+
 })();
